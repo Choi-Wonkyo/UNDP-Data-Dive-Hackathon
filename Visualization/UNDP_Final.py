32586cb314ea62be1e1227c4567186f0e31d702f
Choi-Wonkyo <166977763+Choi-Wonkyo@users.noreply.github.com>
2025-12-16 07:44:14 +0900

    Update UNDP_Final.py

--- Visualization/UNDP_Final.py
+++ Visualization/UNDP_Final.py
@@ -474,7 +474,7 @@ def dashboard_page():
             xaxis_title="Horizon (Years)",
             yaxis_title="Δ Life Expectancy",
             template="plotly_white",
-            height=650
+            height=500
         )
         st.plotly_chart(fig, use_container_width=True)
 
@@ -627,6 +627,7 @@ with st.sidebar:
 
 # ====== Dashboard 실행 ======
 dashboard_page()    
+
 
 
 
